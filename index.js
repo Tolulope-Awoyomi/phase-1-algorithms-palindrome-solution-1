2e43bccefd30262ec786ce5be5e856d21b6a48cb
+ <+>
2022-12-29 20:53:22 -0500

    commit tree 73569d368f77f4b1abb7d8984489d1e77d7bc120

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordValue = word.split('');
+  const reverseWordValue = wordValue.reverse();
+  const reverseWord = reverseWordValue.join('');
+  if (word === reverseWord) {
+    return true
+  }
+  else {
+    return false
+  }
 }
+isPalindrome("abba")
+
 
 /* 
   Add your pseudocode here
+  create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
 */
 
 /*
@@ -12,9 +26,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
